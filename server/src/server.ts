3309834283bcca46497a077a45f2e97e6a5b1cfe
Pallavi <pallavilpmt1995@gmail.com>
2024-12-24 13:55:46 +0530

    backend check1

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import { JWT_SECRET } from "./config";
 import { UserModel, ContentModel, LinkModel } from "./db/db";
 import {authMiddleware} from "./middleware/middleware"
-
+import { random } from "./utils";
 const app=express();
 app.use(express.json());
 app.use(cors());
@@ -85,8 +85,104 @@ app.get("/api/v1/content", authMiddleware, async (req, res)=>{
     res.json({
         content
     })
+})
+
+app.delete("/api/v1/content", authMiddleware, async(req, res)=>{
+    const contentId=req.body.contentId;
+
+    await ContentModel.deleteMany({
+        contentId,
+        //@ts-ignore
+        userId:req.userId
+    })
+
+    res.json({
+        message: "Content Deleted"
+    })
+})
+
+
+// TO DO: Implement delete account/user's all data
+
+
+app.post("/api/v1/brain/share", authMiddleware, async(req, res)=>{
+    const share=req.body.share;
+    
+    if(share){
+        const existingLink=await LinkModel.findOne({
+            //@ts-ignore
+            userId:req.userId
+        })
+
+        if(existingLink){
+            res.json({
+                hash: existingLink.hash
+            })
+            return;
+        }
+
+        const hash=random(10);
+
+        await LinkModel.create({
+            //@ts-ignore
+            userId:req.userId,
+            hash:hash
+        })
+
+        res.json({
+            hash
+        })
+    }else{
+        await LinkModel.deleteOne({
+            //@ts-ignore
+            userId:req.userId,
+        })
+        res.json({
+            message:"Removed link"
+        })
+    }
+})
+
+
+app.get("/api/v1/brain/:shareLink", async(req, res)=>{
+    const hash=req.params.shareLink;
+
+    const link=await LinkModel.findOne({
+        hash
+    });
+
+    if(!link){
+        res.status(411).json({
+            message:"Sorry incorrect input"
+        })
+        return
+    }
+
+    // userId
+    const content=await ContentModel.find({
+        userId: link.userId
+    })
+
+    const user=await UserModel.findOne({
+        _id:link.userId
+    })
+
+    if (!user) {
+        res.status(411).json({
+            message: "user not found, error should ideally not happen"
+        })
+        return;
+    }
+
+    res.json({
+        username: user.username,
+        content: content
+    })
 
 })
+
+
+
 
 
 app.listen(3000);
